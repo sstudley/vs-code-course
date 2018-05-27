80f0b9d25ed5a4faef9ddc638a743160e1021099
Steve Studley <sstudley1@gmail.com>
2018-05-27 11:21:40 -0400

    bunch of stuff

---
+++ test.php
@@ -0,0 +1,14 @@
+<?php
+
+class Person{
+
+  public $name = 'chris';
+  public $username = 'chrisoncode';
+
+  public function doNinjaKick($type = 'front'){
+
+  }
+}
+
+$chris = new Person;
+$chris ->doNinjaKick();
