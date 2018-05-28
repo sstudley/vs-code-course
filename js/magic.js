d08d48d2abc9aec529213f56931b1d299ce851b2
Steve Studley <sstudley1@gmail.com>
2018-05-27 21:37:54 -0400

    change

--- js/magic.js
+++ js/magic.js
@@ -1,11 +1,29 @@
 const person = {
- name:         'chris',
- username:     'chrisoncode',
+  name:         'chris',
+  username:     'chrisoncode',
 };
+
+
 
 console.log(person.name);
 
 
-const myVar        = `blah ${person.name}`;
+let myVar        = `blah ${person.name}`;
 const myNumber     = 3;
-const myLongString = 'here is the loooooonnnnggg string';
+const myLongString = ' here is the loooooonnnnggg string ';
+/**
+ * This function does something
+ * @param {string} first  My description
+ * @param {*string} second Next description
+ */
+function doSomething(first, second){
+    return myVar;
+};
+
+
+doSomething();
+console.log(myVar);
+
+myVar = 'something else ';
+
+console.log(myNumber + myLongString + myVar);
