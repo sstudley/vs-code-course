e643a8fda7a9175d1da630de194579c457648065
Steve Studley <sstudley1@gmail.com>
2018-05-27 20:21:20 -0400

    js to json

--- build/js/magic.js
+++ build/js/magic.js
@@ -1,11 +1,18 @@
 const person = {
- name:         'chris',
- username:     'chrisoncode',
+  name:         'chris',
+  username:     'chrisoncode',
 };
 
 console.log(person.name);
 
 
-const myVar        = `blah ${person.name}`;
+let myVar        = `blah ${person.name}`;
 const myNumber     = 3;
 const myLongString = 'here is the loooooonnnnggg string';
+
+console.log(myVar);
+
+myVar = 'something else';
+
+console.log(myNumber + myLongString);
+
